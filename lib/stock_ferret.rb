991aeb09e3edc5893b2580f83c78d76b0d7f0386
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-22 21:04:19 -0400

    Switch to params on initialize

--- lib/stock_ferret.rb
+++ lib/stock_ferret.rb
@@ -3,26 +3,34 @@ require 'ferrety_ferret'
 
 module Ferrety
   class StockFerret < Ferret
+    attr_accessor :symbol, :instruction, :low_threshold, :high_threshold
 
-    def search(symbol, instruction, low_threshold, high_threshold)
-      watched_data = parse_instruction(symbol, instruction)
+    def initialize(params)
+      @symbol = params[:zip]
+      @instruction = params[:term]
+      @low_threshold = params[:low_threshold].to_i
+      @high_threshold = params[:high_threshold].to_i
+      super
+    end
+
+    def search
       if watched_data.to_f < low_threshold || watched_data.to_f > high_threshold
-        add_parsed_alert(symbol, instruction, watched_data)
+        add_parsed_alert
       end
       @alerts
     end
 
     private
 
-    def add_parsed_alert(symbol, instruction, watched_data)
-      phrase = instruction.upcase == "CHANGE" ? "has changed by" : "is trading at"
-      add_alert("The stock #{symbol} #{phrase} #{watched_data}.")
+    def add_parsed_alert
+      phrase = @instruction.upcase == "CHANGE" ? "has changed by" : "is trading at"
+      add_alert("The stock #{@symbol} #{phrase} #{watched_data}.")
     end
 
-    def parse_instruction(symbol, instruction)
-      case instruction.upcase
-      when "CHANGE" then MarketBeat.percent_change(symbol)
-      else MarketBeat.last_trade_real_time(symbol)
+    def watched_data
+      case @instruction.upcase
+      when "CHANGE" then MarketBeat.percent_change(@symbol)
+      else MarketBeat.last_trade_real_time(@symbol)
       end
     end
   end
